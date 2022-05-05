b844c27a23ee33cf32836eb0bf2e486d33d4032e
carmelofileccia <carmelofileccia@gmail.com>
2022-05-05 13:05:15 +0200

    fix: home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,9 +7,7 @@ import styles from "./styles.module.scss"
 function Home() {
   const [searchInput, setSearchInput] = useState ("");
 
-  const searchItems = (searchValue) => {
-    setSearchInput(searchValue);
-  };
+
   return (
     <div className={styles.home}>
       <label className={styles.home__label}htmlFor="search">Search by title or categories:</label>
